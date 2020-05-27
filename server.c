dea9c8dd43947462611d4e9a1720761918954991
hujunhao211 <61451559+hujunhao211@users.noreply.github.com>
2020-05-27 22:23:45 +0800

    push
    push

--- server.c
+++ server.c
@@ -282,7 +282,7 @@ void *connection_handler(void *argv){
                         }
                     }
                     printf("compression length before: %d\n",compress_length);
-                    if (message.pay_load_length % 8 != 0){
+                    if ((compress_length % 8) != 0){
                         printf("?\n");
                         int padding = 8 - (message.pay_load_length % 8);
                         compression_message = realloc(compression_message,padding + compress_length);
