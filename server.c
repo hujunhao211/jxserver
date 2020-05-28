e6d499ea3b3ab83602209f69acebfcb683594a25
hujunhao211 <61451559+hujunhao211@users.noreply.github.com>
2020-05-28 20:37:17 +0800

    push
    push

--- server.c
+++ server.c
@@ -373,12 +373,12 @@ void *connection_handler(void *argv){
                     write(data->socket_fd, message.pay_load, message.pay_load_length);
                 }
             } else if(message.header.type_digit == 2){
-                printf("2\n");
+//                printf("2\n");
                 DIR* dir;
                 struct dirent* ent;
                 int pay_load_length = 0;
                 char *respone = calloc(100, sizeof(char));
-                printf("msg:    is     %s\n",data->queue->msg);
+//                printf("msg:    is     %s\n",data->queue->msg);
                 if (message.header.compression_bit == 0){
                     if ((dir = opendir(data->queue->msg)) != NULL){
                         while ((ent = readdir(dir)) != NULL) {
@@ -410,15 +410,15 @@ void *connection_handler(void *argv){
                 }
                 free(respone);
             } else if(message.header.type_digit == 4){
-                printf("4\n");
+//                printf("4\n");
             } else if(message.header.type_digit == 6){
-                printf("6\n");
+//                printf("6\n");
             } else if(message.header.type_digit == 8){
-                printf("8\n");
+//                printf("8\n");
                 data->queue->shutdown_flag = 1;
                 break;
             } else {
-                printf("?\n");
+//                printf("?\n");
                 // Send it using exactly the same syscalls as for other file descriptors
                 message.header.type_digit = 0xf;
                 unsigned char header = transform_header(message);
