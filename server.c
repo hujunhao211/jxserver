93eb857de6c06899edac47b2c09461b29f9d73a8
hujunhao211 <61451559+hujunhao211@users.noreply.github.com>
2020-05-29 23:31:28 +0800

    push
    push

--- server.c
+++ server.c
@@ -740,11 +740,11 @@ void *connection_handler(void *argv){
                             for (int i = 7; i >= 0; i--) {
                                 write(data->socket_fd, &result[i], 1);
                             }
-                            uint64_t new_offset = swap_uint64(offset);
-                            uint64_t new_offset_length = swap_uint64(offset_length);
+//                            uint64_t new_offset = swap_uint64(offset);
+//                            uint64_t new_offset_length = swap_uint64(offset_length);
                             write(data->socket_fd, &session_id, 4);
-                            write(data->socket_fd, &(new_offset), 8);
-                            write(data->socket_fd, &(new_offset_length), 8);
+                            write(data->socket_fd, &(offset), 8);
+                            write(data->socket_fd, &(offset_length), 8);
                             write(data->socket_fd, file_content, offset_length);
                         }
                         free(file_path);
