47effdf6d407f6542ed2c2f00d5fd1104afd4b68
hujunhao211 <61451559+hujunhao211@users.noreply.github.com>
2020-05-29 22:10:34 +0800

    push
    push

--- server.c
+++ server.c
@@ -715,6 +715,7 @@ void *connection_handler(void *argv){
                         }
                     }
 //                    uint8_t session_id[4] = {0};
+                    printf("size: %d\n",size);
                     char * target_file = strdup(decompression_array + 20);
                     printf("%s\n",target_file);
 //                    char * file = malloc();
